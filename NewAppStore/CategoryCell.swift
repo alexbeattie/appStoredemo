f40ab8a33e77a829e342eeeccf2d0769e20c9ff1
Alex Beattie <artisanbranding@me.com>
2017-09-22 09:59:07 -0700

    Cells within Cells CollectionView

---
+++ NewAppStore/CategoryCell.swift
@@ -0,0 +1,99 @@
+//
+//  CategoryCell.swift
+//  NewAppStore
+//
+//  Created by Alex Beattie on 9/22/17.
+//  Copyright © 2017 Artisan Branding. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UICollectionViewCell, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource {
+
+    private let cellId = "appCellId"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // a CollectionView INSIDE the cell
+    let appsCollectionView: UICollectionView = {
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = UIColor.blue
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false 
+        return collectionView
+    }()
+    
+    
+    
+    func setupViews() {
+        backgroundColor = UIColor.black
+        
+        
+        addSubview(appsCollectionView)
+        appsCollectionView.dataSource = self
+        appsCollectionView.delegate = self
+        appsCollectionView.register(AppCell.self, forCellWithReuseIdentifier: cellId)
+        
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": appsCollectionView]))
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 150, height:frame.height)
+    }
+    
+}
+
+class AppCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setupViews() {
+        backgroundColor = UIColor.red
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- NewAppStore/ViewController.swift
+++ NewAppStore/ViewController.swift
@@ -33,17 +33,3 @@ class FeaturedAppsController: UICollectionViewController, UICollectionViewDelega
 }
 
 
-class CategoryCell: UICollectionViewCell {
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func setupViews() {
-        backgroundColor = UIColor.black
-    }
-}
